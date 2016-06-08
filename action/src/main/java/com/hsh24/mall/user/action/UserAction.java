bf9a709ef3f47b75a63e84600757642b38f1f0bb
Jiakun Xu <jiakunxu@gmail.com>
2016-06-08 15:46:51 +0800

    <>?

---
+++ action/src/main/java/com/hsh24/mall/user/action/UserAction.java
@@ -0,0 +1,22 @@
+package com.hsh24.mall.user.action;
+
+import com.hsh24.mall.framework.action.BaseAction;
+
+/**
+ * 
+ * @author JiakunXu
+ * 
+ */
+public class UserAction extends BaseAction {
+
+	private static final long serialVersionUID = -7752149287093239697L;
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String detail() {
+		return SUCCESS;
+	}
+
+}
