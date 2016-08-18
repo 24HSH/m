1f23e7bbddb1f4a1fc7077d33aa225d463caf3b7
Jiakun Xu <jiakunxu@gmail.com>
2016-08-18 15:52:17 +0800

    test

--- web/src/main/webapp/statics/js/homepage.js
+++ web/src/main/webapp/statics/js/homepage.js
@@ -557,7 +557,7 @@ function item_list_scan() {
 }
 
 myApp.onPageInit('member.index', function(page) {
-			member_index_stats();
+			// member_index_stats();
 		});
 
 function member_index_stats() {
@@ -627,65 +627,60 @@ function getBrandWCPayRequest(data) {
 }
 
 myApp.onPageInit('trade.list', function(page) {
-			// 下拉刷新页面
-			var ptrContent = $$('.pull-to-refresh-content');
+	// 下拉刷新页面
+	var ptrContent = $$('.pull-to-refresh-content');
 
-			// 添加'refresh'监听器
-			ptrContent.on('refresh', function(e) {
-						// 模拟1s的加载过程
-						setTimeout(function() {
-									// 列表元素的HTML字符串
-									var itemHTML = '';
-									// 前插新列表元素
-									// ptrContent.find('.more').prepend(itemHTML);
-									// 加载完毕需要重置
-									myApp.pullToRefreshDone();
-								}, 1000);
-					});
+	// 添加'refresh'监听器
+	ptrContent.on('refresh', function(e) {
+				// 模拟1s的加载过程
+				setTimeout(function() {
+							// 列表元素的HTML字符串
+							var itemHTML = '';
+							// 前插新列表元素
+							// ptrContent.find('.more').prepend(itemHTML);
+							// 加载完毕需要重置
+							myApp.pullToRefreshDone();
+						}, 1000);
+			});
 
-			$$('form.ajax-submit.trade-list-cancel').on('beforeSubmit',
-					function(e) {
-					});
+	$$('form.ajax-submit.trade-list-cancel').on('beforeSubmit', function(e) {
+			});
 
-			$$('form.ajax-submit.trade-list-sign').on('beforeSubmit',
-					function(e) {
-					});
+	$$('form.ajax-submit.trade-list-sign').on('beforeSubmit', function(e) {
+			});
 
-			$$('form.ajax-submit.trade-list-cancel').on('submitted',
-					function(e) {
-						myApp.hideIndicator();
-						var xhr = e.detail.xhr;
-						myApp.alert(xhr.responseText, '信息', function() {
-									member_index_stats();
-									myApp.getCurrentView().router.back();
-								});
-					});
+	$$('form.ajax-submit.trade-list-cancel').on('submitted', function(e) {
+				myApp.hideIndicator();
+				var xhr = e.detail.xhr;
+				myApp.alert(xhr.responseText, '信息', function() {
+							member_index_stats();
+							myApp.getCurrentView().router.back();
+						});
+			});
 
-			$$('form.ajax-submit.trade-list-sign').on('submitted', function(e) {
-						myApp.hideIndicator();
-						var xhr = e.detail.xhr;
-						myApp.alert(xhr.responseText, '信息', function() {
-									member_index_stats();
-									myApp.getCurrentView().router.back();
-								});
-					});
+	$$('form.ajax-submit.trade-list-sign').on('submitted', function(e) {
+				myApp.hideIndicator();
+				var xhr = e.detail.xhr;
+				myApp.alert(xhr.responseText, '信息', function() {
+							member_index_stats();
+							myApp.getCurrentView().router.back();
+						});
+			});
 
-			$$('form.ajax-submit.trade-list-cancel').on('submitError',
-					function(e) {
-						myApp.hideIndicator();
-						var xhr = e.detail.xhr;
-						myApp.alert(xhr.responseText, '错误');
-					});
+	$$('form.ajax-submit.trade-list-cancel').on('submitError', function(e) {
+				myApp.hideIndicator();
+				var xhr = e.detail.xhr;
+				myApp.alert(xhr.responseText, '错误');
+			});
 
-			$$('form.ajax-submit.trade-list-sign').on('submitError',
-					function(e) {
-						myApp.hideIndicator();
-						var xhr = e.detail.xhr;
-						myApp.alert(xhr.responseText, '错误');
-					});
+	$$('form.ajax-submit.trade-list-sign').on('submitError', function(e) {
+				myApp.hideIndicator();
+				var xhr = e.detail.xhr;
+				myApp.alert(xhr.responseText, '错误');
+			});
 
-			trade_list_stats();
-		});
+		// trade_list_stats();
+	});
 
 function trade_list(type, code) {
 	myApp.getCurrentView().router.load({
