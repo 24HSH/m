b9e066f9624065ff392d88fe26089c8ee5b40c23
Jiakun Xu <jiakunxu@gmail.com>
2016-08-23 16:49:50 +0800

    ...

--- web/src/main/webapp/statics/js/homepage.js
+++ web/src/main/webapp/statics/js/homepage.js
@@ -948,3 +948,35 @@ function user_address_create() {
 
 	$$('#user/address/create').trigger("submit");
 }
+
+myApp.onPageInit('user.address.detail', function(page) {
+	$$('form.ajax-submit.user-address-detail-form').on('beforeSubmit',
+			function(e) {
+			});
+
+	$$('form.ajax-submit.user-address-detail-form').on('submitted',
+			function(e) {
+				myApp.hideIndicator();
+				var xhr = e.detail.xhr;
+				myApp.getCurrentView().router.back({
+					url : myApp.getCurrentView().history[myApp.getCurrentView().history.length
+							- 2],
+					force : true,
+					ignoreCache : true
+				});
+			});
+
+	$$('form.ajax-submit.user-address-detail-form').on('submitError',
+			function(e) {
+				myApp.hideIndicator();
+				var xhr = e.detail.xhr;
+				myApp.alert(xhr.responseText, '错误');
+			});
+
+});
+
+function user_address_detail_update() {
+	myApp.showIndicator();
+
+	$$('#user/address/detail/update').trigger("submit");
+}
